143fb2917dfcb7c83e72740fabaf648bc4a02592
Pasan Premaratne <pasan@premaratne.me>
2019-04-11 21:52:47 -0400

    S1V5

--- SwiftBasics.playground/Contents.swift
+++ SwiftBasics.playground/Contents.swift
@@ -1,1 +1,9 @@
+/*
+ Constants
+ 
+ 1. let is used to declare a constant
+ 2. the word following let is the name given to the constant
+ 3. the assignment operator is used to assign data on the right
+ to the named constant on the left
+ */
 let language = "Swift"
