14af5f85fba4b11783f1672288537f4e817dc0c8
Pasan Premaratne <pasan@premaratne.me>
2019-04-17 21:31:47 -0400

    S3V1

---
+++ SwiftBasics.playground/Pages/Numbers and Booleans.xcplaygroundpage/Contents.swift
@@ -0,0 +1,19 @@
+// Integers (Int)
+// Used to represent whole numbers
+
+let swiftReleaseYear = 2014
+
+/*
+ Floating Point Numbers
+ 
+ - Used to represent decimal values or fractions
+ - Can be represented using two types
+    - Double. For numbers with up to 15 decimal digits
+    - Float. For numbers with as little as 6 decimal digits
+ - Default to using Double
+ */
+let version = 5.0
+
+// Boolean (Bool)
+// Used to model truth using true and false
+let isAwesome = true
