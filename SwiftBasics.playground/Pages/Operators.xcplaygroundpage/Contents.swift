77c20465dc8ad8d1a3a9b81ed62f93fac6941ac8
Pasan Premaratne <pasan@premaratne.me>
2019-04-22 22:14:28 -0400

    S3V3

--- SwiftBasics.playground/Pages/Operators.xcplaygroundpage/Contents.swift
+++ SwiftBasics.playground/Pages/Operators.xcplaygroundpage/Contents.swift
@@ -19,9 +19,39 @@ let width = 10.0
 let area = height * width
 let areaInMeters = area/10.764
 
+// Assignment Operator
 
+let language = "Swift"
 
+let greeting = "Hi, my name is " + "Pasan"
 
+/*
+ Comparison Operators
+ 
+ - Compare the identity of two values
+ - Boolean result
+ */
+
+// Equals Operator
+// ==
+"Swift" == "Swift"
+
+let color = "Blue"
+let alternateColor = "blue"
+
+print(color == alternateColor)
+
+// Not Equals Operator
+// Opposite of the equals operator
+// !=
+
+color != alternateColor
+
+// Greater Than Operator
+// Checks if one value is greater than another
+
+1 > 2
+"a" > "b"
 
 
 
