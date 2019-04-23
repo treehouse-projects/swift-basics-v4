98ac1dadae6db7d3a138c8ec1318a3732712e8d1
Pasan Premaratne <pasan@premaratne.me>
2019-04-22 22:42:04 -0400

    S4V2

---
+++ SwiftBasics.playground/Pages/Type Safety.xcplaygroundpage/Contents.swift
@@ -0,0 +1,11 @@
+// Type Safety
+
+var swiftVersion = "4.2"
+//swiftVersion = 5.0
+
+// Type Inference
+
+let bestPlayer: String = "LeBron James"
+let averagePointsPerGame: Double = 27.2
+let currentSeason: Int = 16
+let hallOfFame: Bool = true
