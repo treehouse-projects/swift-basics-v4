11580babe24bff318c038d339ae4647812a4d925
Pasan Premaratne <pasan@premaratne.me>
2019-04-22 22:34:16 -0400

    S4V1

---
+++ SwiftBasics.playground/Pages/Variables.xcplaygroundpage/Contents.swift
@@ -0,0 +1,17 @@
+// Variables
+
+// Declare a new variable with the keyword var
+var age = 30
+// Reuse an existing variable in an assignment operation to reassign new data
+age = 29
+
+age = age + 1
+
+// Addition Assignment Operator
+age += 1
+print(age)
+
+// Subtraction Assignment Operator
+age -= 1
+
+
